011cc640ac8f7d82b2cbff19cf652dd55b34b619
SharathPradeep <sharathpradeep2001@gmail.com>
2021-02-09 09:48:02 +0530

    Added redirect to homepage if signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch ,Route} from 'react-router-dom';
+import {Switch ,Route,Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
@@ -30,7 +30,7 @@ class App extends React.Component {
           });
         });
       }else{
-        setCurrentUser(null);
+        setCurrentUser(userAuth);
       }
       
     });
@@ -47,7 +47,16 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route exact path='/shop' component={ShopPage}/>
-          <Route exact path='/signin' component={SignInAndSignUpPage}/>
+          <Route exact 
+                 path='/signin' 
+                 render=
+                        {
+                          ()=>
+                              this.props.currentUser?(<Redirect to='/'/>)
+                              :
+                              (<SignInAndSignUpPage/>)
+                        }
+          />
         </Switch>
       </div>
     );
@@ -55,8 +64,12 @@ class App extends React.Component {
   
 }
 
+const mapStateToProps=({user})=>({
+  currentUser:user.currentUser
+})
+
 const mapDispatchToProps = dispatch =>({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import {createStore ,applyMiddleware} from 'redux';
-import logger from 'redux-logger';
+import {logger} from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
